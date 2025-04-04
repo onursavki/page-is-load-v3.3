686f5945da47bd2b4db6f32c13eaf7563271220f
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-04-04 22:43:10 +0300

    page-is-load.js

--- page-is-load.js
+++ page-is-load.js
@@ -1,14 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Yükleyici ve içerik elementlerini al
-    const loader = document.getElementById("loader");
-    const content = document.getElementById("content");
-
-    // Eğer loader varsa, onu gizle
-    if (loader) {
-        setTimeout(() => {
-            // Bekleme süresi ekleyerek daha akıcı geçiş sağlar
-            loader.classList.add("hidden"); 
-            document.body.classList.remove("loading");
-        }, 500); // 0.5 saniye gecikme (isteğe bağlı artırılabilir)
-    }
-});
+  window.addEventListener("load", function () {
+    const preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 500);
+  });
